4261f742649a7e3b04898b966614b9bdf2880cf2
Shafikur Rahman <shafikshaon@gmail.com>
2023-02-26 02:01:19 +0600

    Separate Apps in Separate File

---
+++ bite/config/apps.py
@@ -0,0 +1,17 @@
+__author__ = "Shafikur Rahman"
+
+# Applications
+DJANGO_DEFAULT_APPS = [
+    "django.contrib.auth",
+    "django.contrib.admin",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+CUSTOM_APPS = [
+    "gist",
+]
+
+SITE_ID = 1
