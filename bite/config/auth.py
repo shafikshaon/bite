c6be7cc9dd924f19f4ca5e821059b5cf759ad914
Shafikur Rahman <shafikshaon@gmail.com>
2023-03-13 02:01:44 +0600

    Separate Auth in Separate File

---
+++ bite/config/auth.py
@@ -0,0 +1,20 @@
+__author__ = "Shafikur Rahman"
+# Password validation
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 5,
+        },
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
