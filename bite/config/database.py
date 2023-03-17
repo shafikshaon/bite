0384ad25fdecc80a2e4621daba488de8b897b200
Shafikur Rahman <shafikshaon@gmail.com>
2023-03-18 02:02:10 +0600

    Separate Database in Separate File

---
+++ bite/config/database.py
@@ -0,0 +1,16 @@
+__author__ = "Shafikur Rahman"
+# Database
+
+from decouple import config
+
+# Postgres
+DATABASES = {
+    "default": {
+        "ENGINE": config("DB_ENGINE"),
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
+    }
+}
