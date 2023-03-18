fd9fa1880d16b13fc15fafdc38e3364a25bc5ada
Shafikur Rahman <shafikshaon@gmail.com>
2023-03-19 02:04:10 +0600

    Separate Internationalization in Separate File

---
+++ bite/config/internationalization.py
@@ -0,0 +1,15 @@
+__author__ = "Shafikur Rahman"
+
+from decouple import config
+
+# Internationalization
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = config("TIME_ZONE", default="UTC")
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
