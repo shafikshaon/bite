28188b138752db492fb7dcf69ffc6ff881c4c1aa
Shafikur Rahman <shafikshaon@gmail.com>
2023-03-20 02:04:42 +0600

    Separate Middleware in Separate File

---
+++ bite/config/middleware.py
@@ -0,0 +1,13 @@
+__author__ = "Shafikur Rahman"
+# Middleware
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "gist.middleware.stats_middleware.StatsMiddleware",
+]
