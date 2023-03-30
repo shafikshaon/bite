eaa4bbae93c690168f3d7dbfe3d2e33d5f513ecb
Shafikur Rahman <shafikshaon@gmail.com>
2023-03-31 02:06:27 +0600

    Import Config in settings.py

---
+++ bite/config/__init__.py
@@ -0,0 +1,11 @@
+__author__ = "Shafikur Rahman"
+
+from .apps import *  # noqa
+from .auth import *  # noqa
+from .database import *  # noqa
+from .email import *  # noqa
+from .internationalization import *  # noqa
+from .middleware import *  # noqa
+from .security import *  # noqa
+from .staticfiles import *  # noqa
+from .templates import *  # noqa
