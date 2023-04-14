e2777b05165bde4d67f69ca7d641f46051dc489f
Shafikur Rahman <shafikshaon@gmail.com>
2023-04-14 21:35:43 +0600

    Add UserViewSet in Views

---
+++ users/views.py
@@ -0,0 +1,41 @@
+__author__ = "Shafikur Rahman"
+from rest_framework import mixins, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from users.models.users import User
+from users.permissions import IsUserOrReadOnly
+from users.serializers.users import CreateUserSerializer, UserSerializer
+
+
+class UserViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    """
+    Creates, Updates and Retrieves - User Accounts
+    """
+
+    queryset = User.objects.all()
+    serializers = {"default": UserSerializer, "create": CreateUserSerializer}
+    permissions = {"default": (IsUserOrReadOnly,), "create": (AllowAny,)}
+
+    def get_serializer_class(self):
+        return self.serializers.get(self.action, self.serializers["default"])
+
+    def get_permissions(self):
+        self.permission_classes = self.permissions.get(self.action, self.permissions["default"])
+        return super().get_permissions()
+
+    @action(detail=False, methods=["get"], url_path="me", url_name="me")
+    def get_user_data(self, instance):
+        try:
+            return Response(
+                UserSerializer(self.request.user, context={"request": self.request}).data,
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return Response({"error": "Wrong auth token" + e}, status=status.HTTP_400_BAD_REQUEST)
