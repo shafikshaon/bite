5ea46437d90a363b82bffff234f86b7640ea7b48
Shafikur Rahman <shafikshaon@gmail.com>
2023-04-17 00:23:48 +0600

    Add Users Create Router Add

--- bite/urls.py
+++ bite/urls.py
@@ -1,1 +1,19 @@
-urlpatterns = []
+__author__ = "Shafikur Rahman"
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from users.urls import users_router
+
+router = DefaultRouter()
+
+router.registry.extend(users_router.registry)
+
+urlpatterns = [
+    # api
+    path("api/v1/", include(router.urls)),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
