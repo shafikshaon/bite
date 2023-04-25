f389efa9b69372944158a2e7d73df4f1ae2e7312
Shafikur Rahman <shafikshaon@gmail.com>
2023-04-26 01:04:03 +0600

    Add Login and Refresh Token Api

--- bite/urls.py
+++ bite/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.urls import users_router
 
@@ -12,8 +13,9 @@ router = DefaultRouter()
 router.registry.extend(users_router.registry)
 
 urlpatterns = [
-    # api
     path("api/v1/", include(router.urls)),
+    path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
